686d394dd77cd1ec77b421449707f259216335d0
PaulGeneve <geneve.paul1295@gmail.com>
2021-04-07 12:15:54 +0200

    add header

--- php/partials/header.php
+++ php/partials/header.php
@@ -9,6 +9,19 @@ function partials_header($page) {
     <link rel="stylesheet" href="CSS/bootstrap.min.css">
     <title><?php echo ucfirst($page); ?></title>
 </head>
+<header>
+        <nav>
+            <div class="block block1" id="logo">
+               <img id="img_logo" src="asset/logo_larchive.png" alt="LARCHIVE">
+            </div>
+            <div class="block block2" id="cat">
+                <a id="link-stat" href="#">Archive | Statistique</a>
+            </div>
+            <div class="block block3" id="connexion">
+                <button id="btn-connexion" type="button">Connexion</button>
+            </div>
+        </nav>
+</header>
 
 </html>
 <?php
