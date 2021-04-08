cf3910cc0e77478510c1f4fd0a79e8397c7e6454
Infernaton <lubin.pauly@gmail.com>
2021-04-08 11:11:57 +0200

    fix css login page

--- php/views/login.php
+++ php/views/login.php
@@ -1,39 +1,41 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="css/login.css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css%22%3E">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link href="css/login.css" rel="stylesheet" type="text/css">
     </head>
 
 <body>
-    <form action="php/management/verif.php">
-        <div class="login">
-            <h1>Connexion</h1>
-                <label for="username">
-                    <i class="fas fa-user"></i>
-                </label>
-                <input type="text" name="username" placeholder="Prénom" id="username" required>
-                <label for="password">
-                    <i class="fas fa-lock"></i>
-                </label>
-                <input type="password" name="password" placeholder="Mot de passe" id="password" required>
+    
+    <div class="login">
+        <h1>Connexion</h1>
+        <form action="php/management/verif.php">
+            <label for="username">
+                <i class="fas fa-user"></i>
+            </label>
+            <input type="text" name="username" placeholder="Prénom" id="username" required>
+            <label for="password">
+                <i class="fas fa-lock"></i>
+            </label>
+            <input type="password" name="password" placeholder="Mot de passe" id="password" required>
 
-                <?php
-                //If Error from the connexion.php, print the error
-                if (isset($_SESSION['error_message'])) {
-                ?>
-                    <div class="error"><?php echo $_SESSION['error_message']; ?></div>
-                <?php
+            <?php
+            //If Error from the connexion.php, print the error
+            if (isset($_SESSION['error_message'])) {
+            ?>
+                <div class="error"><?php echo $_SESSION['error_message']; ?></div>
+            <?php
                     unset($_SESSION['error_message']);
-                }
-                ?>
-                <input type="submit" value="Connexion">
-        </div>
+            }
+            ?>
+            <input type="submit" value="Connexion">
+        </form>
+    </div>
 
-        <div class="image">
-            <img src="asset/logo_larchive.png" alt="larchive" height="460px" width="460px">
-        </div>
-    </form>
+    <div id="image">
+        <img src="asset/logo_larchive.png" alt="larchive" width="20%">
+    </div>
+    
 </body>
 
 </html>
