e5452aba24d373cc7c2f0802e3804b69d70df2dc
Sory <sorybagayoko5@gmail.com>
2021-04-08 15:11:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 // check auth
 
-require_once "php\\init.php";
-require_once 'php\\partials\\header.php';
+require_once "php/init.php";
+require_once 'php/partials/header.php';
 
 //'pages' and 'delete' are Array declared in init.php and refer to different status
 
@@ -22,11 +22,11 @@ partials_header($page);
 //In case the user want to delete or to Log out with his account
 if (isset($_GET['delete'])){
     if (in_array($_GET['delete'],$del)){
-        require_once 'php\\management\\'.$_GET['delete'].'.php';
+        require_once 'php/management/'.$_GET['delete'].'.php';
         header('Location: ?p=login');
         die;
     }
 }
 
-require_once 'php\\views\\' . $page . '.php';
+require_once 'php/views/' . $page . '.php';
 ?>
