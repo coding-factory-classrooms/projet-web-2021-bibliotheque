e8dc42fb520e5fe4c8382bb1d36390288687d08c
Infernaton <lubin.pauly@gmail.com>
2021-04-08 15:05:46 +0200

    Change sentences in the home page

--- php/views/home.php
+++ php/views/home.php
@@ -3,19 +3,19 @@
 <?php require_once "php/init.php";?>
 
 <body>
-    <h3>Archiver vos Envies !</h3>
+    <h3>Archivez vos Envies !</h3>
         <div class="container">
             <p id="intro">
-                Larchive vous permets de classer, conserver, ranger et analyser le contenu que vous consommez aucours de l’année.
+                Larchive vous permet de classer, conserver, ranger et analyser le contenu que vous consommez durant l’année.
                 <br>
                 <br>
-                Créez vos propre bibliothèques, vos tags et rangements, transformez ces données en statistiques, et tirez en vos conclusions!
+                Créez vos propres bibliothèques, vos tags et rangements, transformez ces données en statistiques, et tirez en vos conclusions!
                 <br>
                 <br>
                 Avec ses bibliothèques pré-faites, sa possibilité de crééer autant de nouvelles bibliothèques que possible,  les possibilités sont infinies.
             </p>
             <a href="#">
-                <button id="login" type="button">Je m'Inscris</button>
+                <button id="login" type="button">Je m'inscris</button>
             </a>
         </div>
 </body>
