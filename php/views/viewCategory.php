5606047e1d57e9d89646772e56644fd778fe96de
PaulGeneve <geneve.paul1295@gmail.com>
2021-04-09 16:30:03 +0200

    add viewCategory CSS optimisation au max

--- php/views/viewCategory.php
+++ php/views/viewCategory.php
@@ -27,6 +27,7 @@ if ($listObjectBeforeFetch != false){
 <!DOCTYPE html>
 <head>
     <script src="https://kit.fontawesome.com/87a0ec3c80.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/viewCategory.css">
 </head>
 <body>
     <div class="container-obj">
