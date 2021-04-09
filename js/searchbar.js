96a61971d94f0e3a4d8dbdf090ed689e7efaa1d0
Infernaton <lubin.pauly@gmail.com>
2021-04-09 16:34:46 +0200

    possibility to search by tags with '.t' before the research

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,22 +1,40 @@
 function search() {
-    // Declare variables
     let a, txtValue;
     let input = document.getElementById('searchBar');
     let filter = input.value.toUpperCase();
 
     let listObjects = document.getElementById("object");
     let objects = listObjects.getElementsByClassName('id');
-
-    // Loop through all list items, and hide those who don't match the search query
     for (let i = 0; i < objects.length; i++) {
         
-        a = objects[i].getElementsByTagName("p")[0];
-        txtValue = a.textContent || a.innerText; //To get the complete title of the course
+        switch (filter.substr(0,2)){
+            case ".T":
+                a = objects[i].getElementsByClassName("tagsObject")[0];
+                txtValue = a.textContent || a.innerText; //To get the complete title of the object
+    
+                if (txtValue.substr(6).toUpperCase().indexOf(filter.substr(3)) <= -1) { //The test if the research and the title match
+                    objects[i].classList.add("hidden");
+                } else {
+                    objects[i].classList.remove("hidden");
+                }
+                break;
 
-        if (txtValue.toUpperCase().indexOf(filter) <= -1) { //The test if the research and the title match
-            objects[i].classList.add("hidden");
-        } else {
-            objects[i].classList.remove("hidden");
+            case ".":
+                break;
+
+            default:
+                a = objects[i].getElementsByClassName("nameObject")[0];
+                txtValue = a.textContent || a.innerText; //To get the complete title of the object
+    
+                if (txtValue.toUpperCase().indexOf(filter) <= -1) { //The test if the research and the title match
+                    objects[i].classList.add("hidden");
+                } else {
+                    objects[i].classList.remove("hidden");
+                }
         }
     }
+    // Loop through all list items, and hide those who don't match the search query
+
+        
+
 }
