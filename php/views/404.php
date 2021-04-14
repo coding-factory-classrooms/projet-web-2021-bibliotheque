1b1701d506b3e816525a66ac3325cafc7ff36267
Sory <sorybagayoko5@gmail.com>
2021-04-14 16:42:27 +0200

    404 page

---
+++ php/views/404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ERROR 404</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:700,900" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css/404.css" />
+</head>
+
+<body>
+
+	<div id="nontrouv">
+		<div class="nontrouv">
+            <img src="asset/logo_larchive.png" height="40%" width="40%">
+			<div class="nontrouv-404">
+				<h1>404</h1>
+                <h2>Page non trouvée</h2>
+			</div>
+			<a href="?p=home">Accueil</a>
+        </div>
+    </div>
+    
+    
+</body>
+
+</html>
